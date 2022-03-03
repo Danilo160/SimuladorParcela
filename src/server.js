3db310096efe9f3ec161d7628592e39c6b5768d7
Danilo160 <danilocorreiaviana@hotmail.com>
2022-03-03 14:31:41 -0300

    Atualização

--- src/server.js
+++ src/server.js
@@ -5,12 +5,13 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use('/public', express.static(__dirname+'/pages/public'));
-res.sendFile(__dirname+'/pages/home.html');
+
 //app.set('view engine', 'ejs');
 //app.set('views', __dirname+'/pages');
-/*app.get('/', (req, res) => {
-    res.render('home');
-});*/
+app.get('/', (req, res) => {
+    //res.render('home');
+    res.sendFile(__dirname+'/pages/home.html');
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server online');
