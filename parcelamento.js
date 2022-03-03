a63376e774aa862729acf956b2f3298f40f9504e
Danilo160 <danilocorreiaviana@hotmail.com>
2022-03-03 01:22:08 -0300

    Alteração JS

--- parcelamento.js
+++ parcelamento.js
@@ -1,3 +1,7 @@
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 function formata(){
 	document.getElementById("produto").value = parseFloat(document.getElementById("produto").value).toFixed(2);
 
